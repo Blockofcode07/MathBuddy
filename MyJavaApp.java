4439f772022afa23d120dc71af5383ee52c61eee
Aditya <adityajawanjal27@gmail.com>
2025-05-13 10:17:13 +0530

    Completed requirements 3 and 6 by Generating random questions, accepting user inputs, calculating answers, providing feedback, and refactored code using methods

--- MyJavaApp.java
+++ MyJavaApp.java
@@ -1,51 +1,118 @@
 import java.util.Scanner;
-
-/*  Instead of writing long, repetitive blocks of boilerplate code in the main method, 
-we can use user-defined methods to make our code cleaner, easier to read, and more maintainable. 
-By creating reusable methods, we reduce duplication, minimize the chance of errors, 
-and improve the overall structure of our program.  */
+import java.util.Random;
 
 public class MyJavaApp {
 
-    // This method prints a formatted math question to the console
-    public static void printQuestion(int num1, String op, int num2) {
-        String question = "What is " + num1 + op + num2 + "? ";
-        System.out.print(question);
+    // Method to greet the user
+    public static void greetUser(String userName){
+        String welcomeMessage = "Hello, " + userName + "! Welcome to the math quiz. You will be provided with 5 questions, and each carries one mark. Good luck!";
+        System.out.println(welcomeMessage);
     }
+    // Method to print result
+    public static void printResult(String userName){
+        String farewellMessage = "Thanks for giving the quiz, " + userName + ". You will get your result soon.";
+        System.out.println(farewellMessage);
+    }
+   
 
-    // This method displays the user's answer and the correct answer
-    public static void answerFeedback(int userAnswer, int correctAnswer) {
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int num1;
+        int num2;
+        String op;
+        String question;
+        String farewellMessage;
+        int userAnswer;
+        int correctAnswer;
+
+        System.out.print("Please enter your name: ");
+        String userName = scanner.nextLine();
+
+        // Make a customized greeting for the user and print it.
+        greetUser(userName);
+        
+        // Make 5 questions.
+
+        // Question 1
+        num1 = random.nextInt(10) + 1; 
+        num2 = random.nextInt(10) + 1; 
+        op = " + ";
+        question = "What is " + num1 + op + num2 + " ?";
+        System.out.println(question);
+        // Input for Question 1
+        System.out.print("Ans: ");
+        userAnswer = scanner.nextInt();
+        // Compute correct answer for Question 1
+        correctAnswer = num1 + num2;
+        // Feedback for Question 1
         System.out.println("Your answer is: " + userAnswer);
         System.out.println("The correct answer is: " + correctAnswer);
+
+        // Question 2
+        num1 = random.nextInt(10) + 1; 
+        num2 = random.nextInt(10) + 1; 
+        op = " - ";
+        question = "What is " + num1 + op + num2 + " ?";
+        System.out.println(question);
+        // Input for Question 2
+        System.out.print("Ans: ");
+        userAnswer = scanner.nextInt();
+        // Compute correct answer for Question 2
+        correctAnswer = num1 - num2;
+        // Feedback for Question 2
+        System.out.println("Your answer is: " + userAnswer);
+        System.out.println("The correct answer is: " + correctAnswer);
+
+        // Question 3
+        num1 = random.nextInt(10) + 1; 
+        num2 = random.nextInt(10) + 1; 
+        op = " / ";
+        question = "What is " + num1 + op + num2 + " ?";
+        System.out.println(question);
+        // Input for Question 3
+        System.out.print("Ans: ");
+        userAnswer = scanner.nextInt();
+        // Compute correct answer for Question 3
+        correctAnswer = (int)(num1 / num2);
+        // Feedback for Question 3
+        System.out.println("Your answer is: " + userAnswer);
+        System.out.println("The correct answer is: " + correctAnswer);
+
+        // Question 4
+        num1 = random.nextInt(10) + 1; 
+        num2 = random.nextInt(10) + 1; 
+        op = " * ";
+        question = "What is " + num1 + op + num2 + " ?";
+        System.out.println(question);
+        // Input for Question 4
+        System.out.print("Ans: ");
+        userAnswer = scanner.nextInt();
+        // Compute correct answer for Question 4
+        correctAnswer = num1 * num2;
+        // Feedback for Question 4
+        System.out.println("Your answer is: " + userAnswer);
+        System.out.println("The correct answer is: " + correctAnswer);
+
+
+        // Question 5
+        num1 = random.nextInt(10) + 1;  
+        num2 = random.nextInt(10) + 1; 
+        op = " + ";
+        question = "What is " + num1 + op + num2 + " ?";
+        System.out.println(question);
+        // Input for Question 5
+        System.out.print("Ans: ");
+        userAnswer = scanner.nextInt();
+        // Compute correct answer for Question 5
+        correctAnswer = num1 + num2;
+        // Feedback for Question 5
+        System.out.println("Your answer is: " + userAnswer);
+        System.out.println("The correct answer is: " + correctAnswer);
+
+
+        // Print a customized farewell message.
+        printResult(userName);
+
     }
-
-    public static void main(String[] args) {
-
-        Scanner scan = new Scanner(System.in);
-
-        int n1 = 6;
-        int n2 = 7;
-
-        printQuestion(n1,"+",n2);
-        int userAnswer = scan.nextInt();
-        int correctAnswer = n1 + n2;   
-        answerFeedback(userAnswer, correctAnswer);    
-        
-        printQuestion(n1,"-",n2);
-        userAnswer = scan.nextInt();
-        correctAnswer = n1 - n2;   
-        answerFeedback(userAnswer, correctAnswer);    
-
-        printQuestion(n1,"*",n2);
-        userAnswer = scan.nextInt();
-        correctAnswer = n1 * n2;   
-        answerFeedback(userAnswer, correctAnswer);  
-
-        printQuestion(n1,"/",n2);
-        userAnswer = scan.nextInt();
-        correctAnswer = (int)(n1 / n2);   
-        answerFeedback(userAnswer, correctAnswer);   
-
-    }
-
 }
